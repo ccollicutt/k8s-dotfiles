eccc5d78810641f2f8808e7ef8d173915fc3deb0
curtis <curtis@serverascode.com>
2021-03-19 09:15:39 -0400

    add ops and logging

--- install.sh
+++ install.sh
@@ -6,21 +6,32 @@ set -o errexit
 function check_for_commands(){
   if ! command -v wget &> /dev/null
   then
-      echo "ERROR: need wget"
+      log error "wget missing"
       exit 1
   fi
 }
 
-function check_os_version(){
-  if ! command -v lsb_release &> /dev/null; then
-    echo "ERROR: lsb_release not found"
-    echo "INFO: only tested on ubuntu bionic and focal"
+function log(){
+  LEVEL=$1
+  MSG=$2
+  echo "$LEVEL" "$MSG"
+  if [ "$LEVEL" = "error" ]; then
+    echo "exiting..."
     exit 1
   fi
+}
 
+function check_os_version(){
+
+  # by default will be false, but script has option to skip
+  if ! command -v lsb_release &> /dev/null; then
+    log error "lsb_release not found, please install it or use ubuntu bionic or focal"
+    log info "only tested on ubuntu bionic and focal"
+    exit 1
+  fi
   OS_VERSION=$(lsb_release -c -s | tr -d '\n')
   if [[ "$OS_VERSION" != "focal" && "$OS_VERSION" != "bionic" ]]; then
-    echo "ERROR: only tested on ubuntu bionic and focal"
+    log error "only tested on ubuntu bionic and focal"
     exit 1
   fi
 }
@@ -48,13 +59,13 @@ function create_bin_dir() {
   fi
 }
 
-# install k7sdotfilesrc file 
+# install k8sdotfilesrc file 
 function install_k8sdotfile(){
   if [[ ! -f "$BIN_DIR"/k8sdotfilesrc ]]; then 
     if cp "$SCRIPT_DIR"/k8sdotfilesrc "$BIN_DIR"/; then
-      echo "INFO: Copied $SCRIPT_DIR/k8sdotfilesrc to $BIN_DIR/k8sdotfilesrc"
+      log info "Copied $SCRIPT_DIR/k8sdotfilesrc to $BIN_DIR/k8sdotfilesrc"
     else 
-      echo "ERROR: failed to copy to bindir"
+      log error "failed to copy to bindir"
       exit 1
     fi
   fi
@@ -63,70 +74,69 @@ function install_k8sdotfile(){
 # install useful binaries, like helm, into the $BIN_DIR
 function install_binaries(){
 
-
   if [[ ! -f "$BIN_DIR"/powerline-go ]]; then 
-    pushd "$BIN_DIR" || exit 1
+    pushd "$BIN_DIR" || log error "pushd fail"
       wget -q "https://dl.k8s.io/release/v1.20.4/bin/linux/amd64/kubectl"
       chmod 755 kubectl
-    popd || exit 1
+    popd || log error "pushd fail"
   fi
 
   if [[ ! -f "$BIN_DIR"/kubectx ]]; then 
-    pushd "$TMP_DIR" || exit 1
+    pushd "$TMP_DIR" || log error "pushd fail"
       wget -q https://github.com/ahmetb/kubectx/releases/download/v0.9.3/kubectx_v0.9.3_linux_x86_64.tar.gz
       tar zxf kubectx_v0.9.3_linux_x86_64.tar.gz
       mv kubectx "$BIN_DIR"/
-    popd || exit 1
+    popd || log error "pushd fail"
   fi 
 
   if [[ ! -f "$BIN_DIR"/kubens ]]; then 
-    pushd "$TMP_DIR" || exit 1
+    pushd "$TMP_DIR" || log error "pushd fail"
       wget -q https://github.com/ahmetb/kubectx/releases/download/v0.9.3/kubens_v0.9.3_linux_x86_64.tar.gz
       tar zxf kubens_v0.9.3_linux_x86_64.tar.gz
       mv kubens "$BIN_DIR"/
-    popd || exit 1
+    popd || log error "pushd fail"
   fi 
 
   if [[ ! -f "$BIN_DIR"/kube-ps1.sh ]]; then 
-    pushd "$BIN_DIR" || exit 1
+    pushd "$BIN_DIR" || log error "pushd fail"
       wget -q https://raw.githubusercontent.com/jonmosco/kube-ps1/master/kube-ps1.sh
-    popd || exit 1
+    popd || log error "pushd fail"
   fi
 
   if [[ ! -f "$BIN_DIR"/helm ]]; then 
-    pushd "$TMP_DIR" || exit 1
+    pushd "$TMP_DIR" || log error "pushd fail"
       HELM_FILE=helm-v3.5.3-linux-amd64.tar.gz
       wget -q "https://get.helm.sh/$HELM_FILE"
       tar zxf "$HELM_FILE"
       mv linux-amd64/helm "$BIN_DIR"/
-    popd || exit 1
+    popd || log error "pushd fail"
   fi
 
   if [[ ! -f "$BIN_DIR"/kustomize ]]; then 
-    pushd "$TMP_DIR" || exit 1
+    pushd "$TMP_DIR" || log error "pushd fail"
       wget -q https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv4.0.5/kustomize_v4.0.5_linux_amd64.tar.gz
       tar zxf kustomize_v4.0.5_linux_amd64.tar.gz
       mv kustomize "$BIN_DIR"/
-    popd || exit 1
+    popd || log error "pushd fail"
   fi
 
   if [[ ! -f "$BIN_DIR"/powerline-go ]]; then 
-    pushd "$BIN_DIR" || exit 1
+    pushd "$BIN_DIR" || log error "pushd fail"
       wget -q https://github.com/justjanne/powerline-go/releases/download/v1.21.0/powerline-go-linux-amd64 \
         -O powerline-go
       chmod 755 powerline-go
-    popd || exit 1
+    popd || log error "pushd fail"
   fi
 }
 
 function install_packages(){
-  # install powerline fonts
   if ! dpkg -l fonts-powerline > /dev/null; then
     if [[ $EUID -ne 0 ]]; then
+      # user is not root, use sudo
       sudo apt update
       sudo apt install -y fonts-powerline
     else
-      # is root
+      # user is root
       apt update
       apt install -y fonts-powerline
     fi
@@ -143,13 +153,21 @@ trap cleanup_tmp EXIT
 #
 
 run_main() {
-  check_os_version
+
+  if [ "$SKIP_OS_CHECK" != "true" ]; then
+    check_os_version
+    install_packages
+  fi
   check_for_commands
   create_bin_dir
   install_binaries
-  install_packages
   install_k8sdotfile
   install_into_bashrc
+
+  echo "***********************************************************************"
+  echo "* Now run ~/.bashrc to setup the prompt and start using k8s-dotprofile "
+  echo "***********************************************************************"
+
 }
 
 # 
@@ -161,6 +179,21 @@ readonly DOT_DIR="$HOME/.k8s-dotfiles"
 readonly BIN_DIR="$DOT_DIR/bin"
 # tmpdir
 readonly TMP_DIR=$(mktemp -d -t k8s-dotfiles-XXXXXXXXXX)
+SKIP_OS_CHECK=false
+
+while getopts ":hs" opt; do
+  case ${opt} in
+    h ) echo "install.sh [-h for help] [-s skip OS check]"
+        exit 0
+      ;;
+    s ) SKIP_OS_CHECK=true
+        log info "skipping os checks"
+      ;;
+    \? ) log error "invalid option"
+        exit 1
+      ;;
+  esac
+done
 
 # Only run main if running from scripts not testing with bats
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
