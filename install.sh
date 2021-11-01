65212f3e57c19a981baf554c45580c9bb3a6e444
curtis <curtis@serverascode.com>
2021-11-01 07:40:00 -0400

    add octant

--- install.sh
+++ install.sh
@@ -322,7 +322,7 @@ function install_binaries(){
       log debug "installing octant"
       wget -q https://github.com/vmware-tanzu/octant/releases/download/v0.24.0/octant_0.24.0_Linux-64bit.tar.gz 
       tar octant_0.24.0_Linux-64bit.tar.gz
-      mv pack "$BIN_DIR"/octant
+      mv octant_0.24.0_Linux-64bit/octant "$BIN_DIR"/octant
       chmod 755 "$BIN_DIR"/octant
     popd || log error "pushd fail"
   fi
